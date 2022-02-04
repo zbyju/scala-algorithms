df1b423dabf0854a3d524f2a1d49784616c92327
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-02-04 12:30:04 +0100

    Add operations on rational numbers

--- src/main/scala/data-structure/math/Rational.scala
+++ src/main/scala/data-structure/math/Rational.scala
@@ -1,21 +1,32 @@
 package `data-structure`.math
 
-class Rational(val n: Int, val d: Int) {
+class Rational(private val n: Int, private val d: Int) {
     require(d != 0)
 
     val gcd: Int = Rational.gcd(n.abs, d.abs)
     val numerator = n / gcd * d.signum
     val denumerator = d.abs / gcd
 
+    // Math operations on rational numbers
     def unary_- = Rational(-numerator, denumerator)
+    def abs = Rational(numerator.abs, denumerator)
+    def inverse = Rational(denumerator, numerator)
+    def +(o: Rational) = {
+        val lcd = Rational.lcd(denumerator, o.denumerator)
+        Rational(numerator * (lcd / denumerator) + o.numerator * (lcd / o.denumerator), lcd)
+    }
+    def -(o: Rational) = this + (-o)
+    def *(o: Rational) = Rational(numerator * o.numerator, denumerator * o.denumerator)
+    def /(o: Rational) = this * (o.inverse)
 
     override def equals(other: Any) = other match {
         case o: Rational => numerator == o.numerator && denumerator == o.denumerator
         case x => false
     }
-    override def toString = s"$numerator/$denumerator"
+    override def toString = if(denumerator == 1) numerator.toString else s"$numerator / $denumerator"
 }
 
 object Rational {
     private def gcd(a: Int, b: Int): Int = if(b == 0) a else gcd(b, a % b)
+    private def lcd(a: Int, b: Int): Int = a * b / gcd(a, b)
 }
--- src/test/scala/data-structure/math/RationalTest.scala
+++ src/test/scala/data-structure/math/RationalTest.scala
@@ -5,16 +5,39 @@ import org.junit.Assert.*
 
 class RationalTest {
     @Test def constructor() = {
-        assertEquals(Rational(2, 4), Rational(1, 2))
-        assertEquals(Rational(-2, 4), Rational(-1, 2))
-        assertEquals(Rational(2, -4), Rational(-1, 2))
+        assertEquals(Rational(1, 2), Rational(2, 4))
+        assertEquals(Rational(-1, 2), Rational(-2, 4))
+        assertEquals(Rational(-1, 2), Rational(2, -4))
+        assertEquals(Rational(-1, 2), Rational(2, -4))
     }
 
     @Test def unaryMinus() = {
-        assertEquals(-Rational(1, 1), Rational(-1, 1))
-        assertEquals(-Rational(1, 2), Rational(-1, 2))
-        assertEquals(-Rational(5, 2), Rational(-5, 2))
-        assertEquals(-Rational(5, 2), Rational(-5, 2))
-        assertEquals(-Rational(0, 2), Rational(0, 2))
+        assertEquals(Rational(-1, 1), -Rational(1, 1))
+        assertEquals(Rational(-1, 2), -Rational(1, 2))
+        assertEquals(Rational(-5, 2), -Rational(5, 2))
+        assertEquals(Rational(-5, 2), -Rational(5, 2))
+        assertEquals(Rational(0, 2), -Rational(0, 2))
+    }
+
+    @Test def abs() = {
+        assertEquals(Rational(1, 2), Rational(1, 2).abs)
+        assertEquals(Rational(1, 2), Rational(-1, 2).abs)
+        assertEquals(Rational(1, 2), Rational(1, -2).abs)
+        assertEquals(Rational(1, 2), Rational(-1, -2).abs)
+    }
+    
+    @Test def inverse() = {
+        assertEquals(Rational(2, 1), Rational(1, 2).inverse)
+        assertEquals(Rational(1, 2), Rational(2, 1).inverse)
+        assertEquals(Rational(-2, 1), Rational(-1, 2).inverse)
+    }
+
+    @Test def plus() = {
+        assertEquals(Rational(2, 3), Rational(1, 3) + Rational(1, 3))
+        assertEquals(Rational(3, 4), Rational(1, 4) + Rational(2, 4))
+        assertEquals(Rational(1, 2), Rational(1, 4) + Rational(1, 4))
+        assertEquals(Rational(1, 1), Rational(1, 2) + Rational(1, 2))
+        assertEquals(Rational(0, 1), Rational(1, 2) + Rational(-1, 2))
+        assertEquals(Rational(-1, 1), Rational(-1, 2) + Rational(-1, 2))
     }
 }
