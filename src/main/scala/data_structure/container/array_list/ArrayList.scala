40798b077923813433f4acc08d12e422809f8311
zbyju <zbynek.jurica@gmail.com>
2022-08-18 23:57:13 +0200

    Add array, stack, queue operations to arraylist

--- src/main/scala/data_structure/container/array_list/ArrayList.scala
+++ src/main/scala/data_structure/container/array_list/ArrayList.scala
@@ -8,10 +8,12 @@ class ArrayList[T: ClassTag] extends Iterator[T] {
   private val growConstant = 2
   private val initialCapacity = 10
   private var capacity = initialCapacity
-  private var array = new Array[T](initialCapacity)
+  private var array = getInitialArray()
   private val test = List()
 
   private var idx = 0
+
+  private def getInitialArray(): Array[T] = new Array[T](initialCapacity)
 
   override def size: Int = lengthVal
   override def hasNext: Boolean = idx < lengthVal
@@ -22,29 +24,71 @@ class ArrayList[T: ClassTag] extends Iterator[T] {
     res
   }
 
-  def insert(value: T): Unit = {
+  // Array operations
+  def apply(idx: Int): T = {
+    if (idx < length && idx >= 0) {
+      return array(idx)
+    } else {
+      return apply(Math.floorMod(idx, length))
+    }
+  }
+
+  def insert(value: T, idx2: Int): Unit = {
+    val idx = Math.min(idx2, length)
     if (length + 1 >= capacity) {
       capacity *= 2
       val biggerArray = new Array[T](capacity * growConstant)
-      for (i <- 0 until length) {
-        biggerArray(i) = array(i)
+      for (i <- 0 until length + 1) {
+        if (i < idx) biggerArray(i) = array(i)
+        else if (i == idx) biggerArray(i) = value
+        else biggerArray(i) = array(i - 1)
       }
       array = biggerArray
+    } else if (idx == length) {
+      array(idx) = value
+    } else {
+      for (i <- idx + 1 until length + 1) {
+        array(i) = array(i - 1)
+      }
+      array(idx) = value
     }
-    array(length) = value
     lengthVal += 1
   }
 
+  def insert(value: T): Unit = insert(value, length)
+
+  def delete(idx: Int): Option[T] = {
+    if (idx < 0 || idx >= length || length <= 0) return None
+
+    var deleted: Option[T] = Some(array(idx))
+    for (i <- idx until length) {
+      array(i) = array(i + 1)
+    }
+    lengthVal -= 1
+    deleted
+  }
+
+  def delete(): Option[T] = delete(length - 1)
+
+  // Stack operations O(1)
+  def push(value: T) = insert(value, length)
+
+  def pop() = delete()
+
+  // Queue operations O(n) enqueue; O(1) dequeue
+  def enqueue(value: T) = insert(value, 0)
+
+  def dequeue() = delete()
+
+  // Misc
   def first = array(0)
 
   def last = array(lengthVal - 1)
 
-  def apply(idx: Int): T = {
-    if (idx < length) {
-      array(idx)
-    } else {
-      last
-    }
+  def deleteAll() = {
+    lengthVal = 0
+    capacity = initialCapacity
+    array = getInitialArray()
   }
 
   override def toString(): String = {
--- src/test/scala/data_structure/container/array_list/ArrayListTest.scala
+++ src/test/scala/data_structure/container/array_list/ArrayListTest.scala
@@ -10,11 +10,49 @@ class ArrayListTest {
     assertEquals(0, al.length)
     assertEquals(true, al.isEmpty)
     assertEquals(Seq(), al.toSeq)
+    assertEquals(None, al.delete())
+    assertEquals(None, al.delete(0))
+    assertEquals(None, al.delete(10))
+    assertEquals(None, al.delete(-1))
 
     al.insert(1)
     al.insert(2)
     al.insert(3)
 
     assertEquals(Seq(1, 2, 3), al.toSeq)
+    assertEquals(1, al(0))
+    assertEquals(2, al(1))
+    assertEquals(3, al(2))
+    assertEquals(3, al(-1))
+    assertEquals(2, al(-2))
+    assertEquals(1, al(-3))
+    assertEquals(1, al(3))
+
+    al.insert(4)
+    al.insert(5)
+    al.insert(6)
+    al.insert(7)
+    al.insert(8)
+    al.insert(9)
+    al.insert(10)
+    al.insert(11)
+
+    assertEquals(Some(11), al.delete())
+    assertEquals(Some(10), al.pop())
+    assertEquals(Some(9), al.dequeue())
+
+    al.deleteAll()
+
+    al.insert(3)
+    al.insert(1, 0)
+    al.insert(2, 1)
+
+    assertEquals(Some(2), al.delete(1))
+    assertEquals(Some(3), al.delete(1))
+    assertEquals(None, al.delete(1))
+    assertEquals(Some(1), al.delete())
+
+    al.insert(10, 100)
+    assertEquals(Some(10), al.delete())
   }
 }
